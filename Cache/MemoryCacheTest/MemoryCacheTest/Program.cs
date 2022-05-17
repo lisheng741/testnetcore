ea65128e62063076d9682f20d7996f2bfb5af1c8
lisheng741 <lisheng741@qq.com>
2022-05-17 22:57:18 +0800

    cache

---
+++ Cache/MemoryCacheTest/MemoryCacheTest/Program.cs
@@ -0,0 +1,29 @@
+using Microsoft.Extensions.Caching.Memory;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddMemoryCache();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.Map("/test", (IMemoryCache cache) =>
+{
+    cache.Set("test", "test123");
+    var test = cache.Get("test");
+    return test;
+});
+
+app.Run();
--- Cache/RedisTest/RedisTest/Controllers/TestController.cs
+++ Cache/RedisTest/RedisTest/Controllers/TestController.cs
@@ -1,5 +1,4 @@
-﻿using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Caching.Distributed;
 using System.Text;
 
@@ -20,7 +19,7 @@ public class TestController : ControllerBase
     public async Task Set(string key, string value, double expiration)
     {
         var encodedValue = Encoding.UTF8.GetBytes(value);
-        await _cache.SetAsync(key, encodedValue,new DistributedCacheEntryOptions() { AbsoluteExpiration = DateTime.Now.AddSeconds(expiration) });
+        await _cache.SetAsync(key, encodedValue, new DistributedCacheEntryOptions() { AbsoluteExpiration = DateTime.Now.AddSeconds(expiration) });
     }
 
     [HttpGet]
