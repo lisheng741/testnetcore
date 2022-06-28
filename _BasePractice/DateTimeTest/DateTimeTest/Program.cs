9db62c2de49c808d9c3e6381f1603949cde80ec2
lisheng741 <lisheng741@qq.com>
2022-06-28 18:48:12 +0800

    DateTime 测试

---
+++ _BasePractice/DateTimeTest/DateTimeTest/Program.cs
@@ -0,0 +1,12 @@
+﻿
+Console.WriteLine(DateTime.Now);
+
+Console.WriteLine(DateTime.UtcNow);
+
+Console.WriteLine("-----------------");
+
+Console.WriteLine(DateTimeOffset.Now);
+
+Console.WriteLine(DateTimeOffset.UtcNow);
+
+Console.ReadLine();
