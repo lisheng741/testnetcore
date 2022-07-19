96ee6d5246fa68b0b949a50eea6a747281333a17
cls <lisheng741@qq.com>
2022-07-19 15:05:48 +0800

    完成读取Summary

--- _BasePractice/ReadSummaryTest/ReadSummaryTest/Models/TestModel.cs
+++ _BasePractice/ReadSummaryTest/ReadSummaryTest/Models/TestModel.cs
@@ -4,21 +4,35 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ReadSummaryTest.Models
+namespace ReadSummaryTest.Models;
+
+/// <summary>
+/// 测试模型
+/// </summary>
+public class TestModel
 {
     /// <summary>
-    /// 测试模型
+    /// 主键
     /// </summary>
-    public class TestModel
-    {
-        /// <summary>
-        /// 主键
-        /// </summary>
-        public int Id { get; set; }
+    public int Id { get; set; }
 
-        /// <summary>
-        /// 名字
-        /// </summary>
-        public string Name { get; set; } = "";
-    }
+    /// <summary>
+    /// 名字
+    /// </summary>
+    public string Name { get; set; } = "";
+
+    /// <summary>
+    /// 内部id
+    /// </summary>
+    private int _id;
+
+    /// <summary>
+    /// 内部名称
+    /// </summary>
+    public string name = "";
+
+    /// <summary>
+    /// 测试方法
+    /// </summary>
+    public void TestMethod() { }
 }
--- _BasePractice/ReadSummaryTest/ReadSummaryTest/Program.cs
+++ _BasePractice/ReadSummaryTest/ReadSummaryTest/Program.cs
@@ -1,2 +1,30 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using ReadSummaryTest.Models;
+using System.Reflection;
+using System.Xml;
+
+Assembly assembly = Assembly.GetExecutingAssembly();
+
+TestModel test = new();
+
+var type = typeof(TestModel);
+
+string summary = type.GetSummary();
+
+// 属性
+var fields = type.GetProperties();
+
+var s1 = fields[0].GetSummary();
+var s2 = fields[1].GetSummary();
+
+// 方法
+MethodInfo[] methodInfos = type.GetMethods();
+var s3 = methodInfos[0].GetSummary();
+
+// Field
+FieldInfo[] fieldInfos = type.GetFields();
+var s4 = fieldInfos[0].GetSummary();
+
+// 成员
+MemberInfo? memberInfo = type.GetMember(fields[0].Name).FirstOrDefault();
+
+Console.ReadLine();
