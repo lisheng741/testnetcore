4707bb760ddbc454ce32434b9cc95c9510a2c32d
cls <lisheng741@qq.com>
2022-08-10 18:33:06 +0800

    数组是IEnumerable的实现

---
+++ _BasePractice/EnumerableTest/EnumerableTest/Program.cs
@@ -0,0 +1,10 @@
+﻿
+Console.WriteLine(typeof(int[]).IsAssignableFrom(typeof(IEnumerable<int>)));
+
+Console.WriteLine(typeof(IList<int>).IsAssignableFrom(typeof(int[])));
+
+Console.WriteLine(typeof(IEnumerable<int>).IsAssignableFrom(typeof(int[])));
+
+Console.WriteLine(typeof(IEnumerable<int>).IsAssignableFrom(typeof(List<int>)));
+
+Console.ReadLine();
