a01862fcbaf0a1f0fd32f44f2d00aa973eaf3e79
lisheng741 <lisheng741@qq.com>
2023-01-12 08:33:01 +0800

    添加 ResultFilter 测试

---
+++ _ASPNETCore/Filter/ResultFilterTest/ResultFilterTest/Filters/UnifyResultFilter.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using ResultFilterTest.Result;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ResultFilterTest.Filters
+{
+    public class UnifyResultFilter : IResultFilter
+    {
+        public void OnResultExecuted(ResultExecutedContext context)
+        {
+        }
+
+        public void OnResultExecuting(ResultExecutingContext context)
+        {
+            var data = context.Result as ObjectResult;
+
+            var result = new AppResult<object>("200", "成功！", data.Value);
+
+            context.Result = new JsonResult(result);
+        }
+    }
+}
---
+++ _ASPNETCore/Filter/ResultFilterTestNet6/Result/AppResult.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ResultFilterTest.Result
+{
+    public class AppResult<T>
+    {
+        public string Code { get; set; }
+
+        public string Message { get; set; }
+
+        public T Data { get; set; }
+
+        public AppResult(string code, string message, T data)
+        {
+            Code = code;
+            Message = message;
+            Data = data;
+        }
+    }
+}
