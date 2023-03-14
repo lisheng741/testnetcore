beb7eca3aca8994856bafea14b657a8146b08bd7
lisheng741 <lisheng741@qq.com>
2023-03-14 18:17:13 +0800

    同步

---
+++ _BasePractice/ExpressionTest/ExpressionLinqTest/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+
+namespace ExpressionLinqTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> list = new List<string>() { "1","2","3","3333"};
+
+            Expression<Func<string, bool>> expr = s => s.Contains("3");
+
+            var result = list.Where(expr.Compile());
+
+            foreach(var s in result)
+            {
+                Console.WriteLine(s);
+            }
+        }
+    }
+}
